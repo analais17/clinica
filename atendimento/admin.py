078906833d63812cf2ddf259deb810b359064298
Your Name <you@example.com>
2019-05-30 20:09:49 -0300

    Cria modelo Medicamento

--- atendimento/admin.py
+++ atendimento/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from . models import Paciente,Medico
+from . models import Paciente,Medico,Medicamento
 
 admin.site.register(Paciente)
 admin.site.register(Medico)
+admin.site.register(Medicamento)
--- atendimento/models.py
+++ atendimento/models.py
@@ -12,3 +12,9 @@ class Medico (models.Model):
     crm = models.CharField(max_length=11)
     especialidade = models.CharField(max_length=50)
     idade= models.IntegerField()
+
+class Medicamento (models.Model):
+    nome = models.CharField(max_length=50)
+    valor= models.DecimalField(max_digits=30,decimal_places=2)
+    gravida=models.BooleanField()
+    
